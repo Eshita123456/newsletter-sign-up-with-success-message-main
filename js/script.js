204f27f523dc10e2b52dc897650a4b1188345495
Eshita Srivastava <141634638+Eshita123456@users.noreply.github.com>
2023-08-08 12:15:28 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector('#form');
+    const emailInput = document.querySelector('#email');
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const errorSpan = document.querySelector('#invalid');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // Verifica se o email está vazio ou inválido
+        if (emailInput.value === "" || !emailRegex.test(emailInput.value)) {
+            errorSpan.innerHTML = "Valid email required";
+            emailInput.parentElement.classList.add('invalid');
+        } else {
+            //Salva o email em userEmail e redireciona o usuário para outra página
+            localStorage.setItem('userEmail', emailInput.value);
+            window.location.href = 'success.html';
+        }
+    });
+
+    emailInput.addEventListener('input', () => {
+        errorSpan.textContent = "";
+        emailInput.parentElement.classList.remove('invalid');
+    });
+});
+  
+
+
+
+
